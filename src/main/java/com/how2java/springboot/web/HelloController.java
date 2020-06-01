905fd6a6d3a3777f5a0f9cd3a7bdf62fb6362bad
C <1124861386@qq.com>
2020-06-01 23:04:45 +0800

    2020.6.1.3

--- src/main/java/com/how2java/springboot/web/HelloController.java
+++ src/main/java/com/how2java/springboot/web/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  
 @Controller
 public class HelloController {
- private int bum;
+ private int bum=10;
     @RequestMapping("/hello")
     public String hello(Model m) {
     	m.addAttribute("name", "thymeleaf");
